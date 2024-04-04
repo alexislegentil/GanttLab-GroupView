6999c64de89cc7032ace31f701be210a23b8f14c
alexislegentil <alexis.romand27@gmail.com>
2024-04-04 15:53:59 +0200

    now the way to receive issues is by project
    next step is to have a PaginatedListOfProject ready to be displayed

---
+++ ganttlab-master 2/packages/ganttlab-entities/src/pagination/PaginatedListOfProjects.ts
@@ -0,0 +1,7 @@
+import { PaginatedList } from "./PaginatedList";
+import { Project } from "../core/Project";
+
+/**
+ * A paginated list of {@link Project}s
+ */
+export class PaginatedListOfProjects extends PaginatedList<Project> {}
--- ganttlab-master 2/packages/ganttlab-gateways/src/views/group/strategies/ViewGroupGitLabStategy.ts
+++ ganttlab-master 2/packages/ganttlab-gateways/src/views/group/strategies/ViewGroupGitLabStategy.ts
@@ -1,71 +1,98 @@
 import {
-    ViewSourceStrategy,
-    Configuration,
-    PaginatedListOfTasks,
-    Task,
-    Group,
-    Filter,
-  } from 'ganttlab-entities';
-  import { GitLabGateway } from '../../../sources/gitlab/GitLabGateway';
-  import { GitLabGroup } from '../../../sources/gitlab/types/GitLabGroup';
-  import { GitLabIssue } from '../../../sources/gitlab/types/GitLabIssue';
-  import {
-    getTaskFromGitLabIssue,
-    getPaginationFromGitLabHeaders,
-    getGroupFromGitLabGroup,
-  } from '../../../sources/gitlab/helpers';
+        ViewSourceStrategy,
+        Configuration,
+        PaginatedListOfTasks,
+        Task,
+        Filter,
+        Project,
+    } from 'ganttlab-entities';
+    import { GitLabGateway } from '../../../sources/gitlab/GitLabGateway';
+    import { GitLabIssue } from '../../../sources/gitlab/types/GitLabIssue';
+    import {
+        getTaskFromGitLabIssue,
+        getPaginationFromGitLabHeaders,
+    } from '../../../sources/gitlab/helpers';
 
-  import { IssuesStateFilter } from '../../../filters/IssuesStateFilter';
-  
-  export class ViewGroupGitLabStrategy
-    implements ViewSourceStrategy<PaginatedListOfTasks> {
-    async execute(
-      source: GitLabGateway,
-      configuration: Configuration,
-      filter: Filter | null,
-    ): Promise<PaginatedListOfTasks> {
-      let stateFilter = null;
-      if (filter instanceof IssuesStateFilter)  {
-        stateFilter = filter.requestGitLabArgs();
-      }
-      console.log('configuration', configuration);
-      const encodedGroup = encodeURIComponent(
-        configuration.group.path as string,
-      );
-      const { data, headers } = await source.safeAxiosRequest<Array<GitLabIssue>>(
-        {
-          method: 'GET',
-          url: `groups/${encodedGroup}/issues`,
-          params: {
-            page: configuration.tasks.page,
-            // eslint-disable-next-line @typescript-eslint/camelcase
-            per_page: configuration.tasks.pageSize,
-            state: stateFilter? stateFilter : 'opened',
-          },
-        },
-      );
-      const tasksList: Array<Task> = [];
-      for (const gitlabIssue of data) {
-        console.log(gitlabIssue);
-        const task = getTaskFromGitLabIssue(gitlabIssue);
-        tasksList.push(task);
-      }
-      tasksList.sort((a: Task, b: Task) => {
-        if (a.due && b.due) {
-          return a.due.getTime() - b.due.getTime();
+    import { IssuesStateFilter } from '../../../filters/IssuesStateFilter';
+import { GitLabProject } from '../../../sources/gitlab/types/GitLabProject';
+    
+    export class ViewGroupGitLabStrategy
+        implements ViewSourceStrategy<PaginatedListOfTasks> {
+        async execute(
+            source: GitLabGateway,
+            configuration: Configuration,
+            filter: Filter | null,
+        ): Promise<PaginatedListOfTasks> {
+            let stateFilter = null;
+            if (filter instanceof IssuesStateFilter)  {
+                stateFilter = filter.requestGitLabArgs();
+            }
+            console.log('configuration', configuration);
+            const encodedGroup = encodeURIComponent(
+                configuration.group.path as string,
+            );
+            const projectsResponse = await source.safeAxiosRequest<Array<GitLabProject>>({
+                method: 'GET',
+                url: `/groups/${encodedGroup}/projects`,
+                params: {
+                    page: configuration.group.page,
+                    // eslint-disable-next-line @typescript-eslint/camelcase
+                    per_page: configuration.group.pageSize,
+             // state: stateFilter? stateFilter : 'opened',
+                 //   archived: false,
+                },
+            });
+            const pagination = getPaginationFromGitLabHeaders(projectsResponse.headers);
+            let tasksForActiveGroup: PaginatedListOfTasks | null = null;
+            const tasksList: Array<Task> = [];
+            for (
+                let projectIndex = 0;
+                projectIndex < projectsResponse.data.length;
+                projectIndex++
+            ) {
+                console.log('project', projectsResponse.data[projectIndex]);
+                const gitlabProject = projectsResponse.data[projectIndex];
+                const encodedProject = encodeURIComponent(
+                    gitlabProject.path_with_namespace as string,
+                );
+    
+                    const { data, headers } = await source.safeAxiosRequest<Array<GitLabIssue>>({
+                        method: 'GET',
+                        url: `/projects/${encodedProject}/issues`,
+                        params: {
+                            page: configuration.tasks.page,
+                            // eslint-disable-next-line @typescript-eslint/camelcase
+                            per_page: configuration.tasks.pageSize,
+                            state: stateFilter? stateFilter : 'opened',
+                          //  group: gitlabProject.name,
+                        },
+                    });
+                   
+                    for (const gitlabIssue of data) {
+                        const task = getTaskFromGitLabIssue(gitlabIssue);
+                        tasksList.push(task);
+                    }
+                    
+                }
+            tasksList.sort((a: Task, b: Task) => {
+                if (a.due && b.due) {
+                    return a.due.getTime() - b.due.getTime();
+                }
+                return 0;
+            });
+
+           
+            tasksForActiveGroup = new PaginatedListOfTasks(
+                tasksList,
+                configuration.tasks.page as number,
+                configuration.tasks.pageSize as number,
+                pagination.previousPage,
+                pagination.nextPage,
+                pagination.lastPage,
+                pagination.total,
+            );
+    
+            return tasksForActiveGroup as PaginatedListOfTasks;
         }
-        return 0;
-      });
-      const gitlabPagination = getPaginationFromGitLabHeaders(headers);
-      return new PaginatedListOfTasks(
-        tasksList,
-        configuration.tasks.page as number,
-        configuration.tasks.pageSize as number,
-        gitlabPagination.previousPage,
-        gitlabPagination.nextPage,
-        gitlabPagination.lastPage,
-        gitlabPagination.total,
-      );
     }
-  }
   
