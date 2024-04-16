cb03790d2b8139139779a5dab0fd2c66ef98cc78
alexislegentil <alexis.romand27@gmail.com>
2024-04-16 12:22:08 +0200

    started style the grid

--- ganttlab-master 2/packages/ganttlab-adapter-webapp/src/components/gateways/charts/legacy/GanttComponent.vue
+++ ganttlab-master 2/packages/ganttlab-adapter-webapp/src/components/gateways/charts/legacy/GanttComponent.vue
@@ -6,6 +6,10 @@
 import {gantt} from 'dhtmlx-gantt';
 
 var dateToStr = gantt.date.date_to_str(gantt.config.task_date);
+var textEditor = {type: "text", map_to: "name"};
+var dateEditor = {type: "date", map_to: "start_date"};
+var durationEditor = {type: "number", map_to: "duration", min:0, max: 100};
+var usersEditor = {type: "text", map_to: "user"};
 
 export default {
   props: {
@@ -20,7 +24,9 @@ export default {
   mounted: function () {
     console.log(this.tasks);
     gantt.plugins({ 
-        marker: true 
+        marker: true,
+        multiselect: true ,
+        tooltip: true 
     }); 
     gantt.config.date_format = "%Y-%m-%d";
     gantt.config.scales = [
@@ -29,17 +35,26 @@ export default {
     ];
     gantt.config.fit_tasks = true;
     gantt.config.columns = [
-    {name: "name", label: "Task name", tree: true, width: 200 },
-    {name: "start_date", label: "Start time", align: "center", width: 150 },
-    {name: "duration", label: "Duration", align: "center", width: 100 },
-    {name: "user", label: "User", align: "center", width: 150 }
+    {name: "name", label: "Task name", tree: true, width: 170, tree : true, resize: true, editor: textEditor },
+    {name: "start_date", label: "Start time", align: "center", width: 150 , resize: true, editor: dateEditor},
+    {name: "duration", label: "Duration", align: "center", width: 50, editor: durationEditor},
+    {name: "user", label: "User", align: "center", width: 100, editor: usersEditor}
     ];
+    gantt.templates.task_text = function(start, end, task) {
+      return "<b>Name:</b> " + task.name + (task.user ? ",<b> Holders:</b> " + task.user : "");
+    };
+    gantt.templates.tooltip_text = function(start, end, task) {
+    // Personnalisez ici le texte du tooltip pour chaque tâche
+    return "<b>" + task.name + "</b><br/>" + "Start: " + gantt.templates.tooltip_date_format(start) + "<br/>" + "End: " + gantt.templates.tooltip_date_format(end) + "<br/>" + (task.user ? "User: " + task.user + "<br/>" : "") + (task.duration ? "Duration: " + task.duration + " days" : "");
+    };
+
     gantt.config.columns_resizable = true;
+    gantt.config.columns_autoresize = true;
     gantt.addMarker({
-    start_date: new Date(), //a Date object that sets the marker's date
-    css: "today", //a CSS class applied to the marker
-    text: "Now", //the marker title
-    title: dateToStr( new Date()) // the marker's tooltip
+      start_date: new Date(), //a Date object that sets the marker's date
+      css: "today", //a CSS class applied to the marker
+      text: "Now", //the marker title
+      title: dateToStr( new Date()) // the marker's tooltip
     });
 
     
